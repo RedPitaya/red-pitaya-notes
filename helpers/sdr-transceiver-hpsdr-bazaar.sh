633fa9fa007f034623f60ae4d29b078c8dac9019
TheChief <thechief@praised-land.de>
2018-12-23 14:30:55 +0100

    DG8MG: First test build for a combined SDR application package working on Z10 and Z20 based STEMlabs.
    Signed-off-by: TheChief <thechief@praised-land.de>

--- helpers/sdr-transceiver-hpsdr-bazaar.sh
+++ helpers/sdr-transceiver-hpsdr-bazaar.sh
@@ -1,15 +1,19 @@
-project=sdr_transceiver_hpsdr
-project1=stemlab_sdr_transceiver_hpsdr
+project=sdr-transceiver-hpsdr
+project_z20=sdr_transceiver_hpsdr_z20
+project_package=stemlab_sdr_transceiver_hpsdr
 server=sdr-transceiver-hpsdr
 
-cp -a projects/$project/bazaar $project1
-arm-linux-gnueabihf-gcc -shared -Wall -fPIC -Os -s $project1/src/main.c -o $project1/controllerhf.so
-arm-linux-gnueabihf-gcc -static -O3 -march=armv7-a -mcpu=cortex-a9 -mtune=cortex-a9 -mfpu=neon -mfloat-abi=hard projects/$project/server/$server.c -D_GNU_SOURCE -Iprojects/$project/server -lm -lpthread -o $project1/$server
-cp tmp/$project.bit $project1
+cp -a projects/$project_z20/bazaar $project_package
+
+arm-linux-gnueabihf-gcc -shared -Wall -fPIC -Os -s $project_package/src/main.c -o $project_package/controllerhf.so
+arm-linux-gnueabihf-gcc -static -O3 -march=armv7-a -mcpu=cortex-a9 -mtune=cortex-a9 -mfpu=neon -mfloat-abi=hard projects/$project_z20/server/$server.c -D_GNU_SOURCE -Iprojects/$project_z20/server -lm -lpthread -o $project_package/$server
+
+cp tmp/$project.bit $project_package
+cp tmp/$project_z20.bit $project_package
 
 build_number=`git rev-list HEAD --count`
 revision=`git log -n1 --pretty=%h`
 
-sed -i "s/REVISION/$revision/; s/BUILD_NUMBER/$build_number/" $project1/info/info.json
+sed -i "s/REVISION/$revision/; s/BUILD_NUMBER/$build_number/" $project_package/info/info.json
 
-zip -r $project1-0.94-$build_number.zip $project1
+zip -r $project_package-0.98-$build_number.zip $project_package
